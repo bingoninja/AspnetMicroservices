d80f1f036a3048fac7b538425fc56039fec6ab9b
Nicholas Williams <nickwilliams19@gmail.com>
2022-03-08 14:31:23 -0500

    Add discount gRPC services

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        public readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productname)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productname };
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
